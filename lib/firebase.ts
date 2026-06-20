6e8331ca436f1ce7451eb23817ed7c73674f4061
mym-dev-ui <saddam.saqqar@icloud.com>
2026-06-20 09:25:44 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,16 +2,12 @@ import { getApp, getApps, initializeApp, FirebaseApp } from "firebase/app";
 import { getDatabase, Database } from "firebase/database";
 import { doc, getFirestore, setDoc, Firestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDBbLp4bZybcrkwVOZqD4-H34AVMHVNpxc",
-  authDomain: "tameenbecer-m.firebaseapp.com",
-  databaseURL: "https://tameenbecer-m-default-rtdb.firebaseio.com",
-  projectId: "tameenbecer-m",
-  storageBucket: "tameenbecer-m.firebasestorage.app",
-  messagingSenderId: "351037299340",
-  appId: "1:351037299340:web:e68887a4b7c1a5348379a9",
-  measurementId: "G-C7R2BP83C8"
-};
+NEXT_PUBLIC_FIREBASE_API_KEY=TEMP_KEY
+NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=TEMP.firebaseapp.com
+NEXT_PUBLIC_FIREBASE_PROJECT_ID=TEMP_PROJECT
+NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=TEMP.appspot.com
+NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=000000000
+NEXT_PUBLIC_FIREBASE_APP_ID=1:000000:web:temp
 
 const isFirebaseConfigured = Boolean(
   firebaseConfig.apiKey &&
